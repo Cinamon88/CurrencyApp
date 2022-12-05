92ca5dbef719dffd0a49ac8fdf97656edfb1390b
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-12-05 11:49:53 +0100

    add test NaN

--- src/utils/__tests__/convertPLNtoUSD.js
+++ src/utils/__tests__/convertPLNtoUSD.js
@@ -1,4 +1,4 @@
-import { convertPLNToUSD } from './../convertPLNtoUSD';
+import { convertPLNToUSD } from '../convertPLNtoUSD';
 
 describe('ConvertPLNtoUSD', () => {
   it('should return proper value when good input', () => {
@@ -6,5 +6,11 @@ describe('ConvertPLNtoUSD', () => {
     expect(convertPLNToUSD(2)).toBe('$0.57');
     expect(convertPLNToUSD(20)).toBe('$5.71');
     expect(convertPLNToUSD(12)).toBe('$3.43');
+  });
+
+  it('should return NaN when input is text', () => {
+    expect(convertPLNToUSD('6')).toBeNaN();
+    expect(convertPLNToUSD('abc')).toBeNaN();
+    expect(convertPLNToUSD('-543')).toBeNaN();
   });
 });
